f8a125ddbf1ad8e7b5747913727876ed0019b457
alexpryp <alexpryp@gmail.com>
2020-06-11 17:41:30 +0300

    Added App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { formatTime } from './additionalFunctions.js';
 
 function App() {
+  const [timeCounter, setTimeCounter] = useState(new Date());
+  const [mainTimer, setMainTimer] = useState(new Date(2020, 0, 1));
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      setMainTimer(new Date(new Date() - timeCounter))
+    }, 100);
+  });
+
+  console.log(mainTimer);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{formatTime(mainTimer)}</p>
     </div>
   );
 }
