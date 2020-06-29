222d769c4a470a7c0863e65442ad6c6af917306f
alexpryp <alexpryp@gmail.com>
2020-06-29 23:39:13 +0300

    Added MainTimer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { formatTime } from './additionalFunctions.js';
 
+function MainTimer() {
+	const [timeCounter, setTimeCounter] = useState(new Date());
+	const [mainTimer, setMainTimer] = useState(new Date(2020, 0, 1));
+
+	useEffect(() => {
+		let timerId = setTimeout(() => {
+			setMainTimer(new Date(new Date() - timeCounter))
+		}, 100);
+
+		return function cleanup() {
+			clearTimeout(timerId);
+		} 
+	});
+
+	return (
+		<div className="App">
+			<p>{formatTime(mainTimer)}</p>
+		</div>
+	);
+}
+
 function App() {
-  const [timeCounter, setTimeCounter] = useState(new Date());
-  const [mainTimer, setMainTimer] = useState(new Date(2020, 0, 1));
-
-  useEffect(() => {
-    let timerId = setTimeout(() => {
-      setMainTimer(new Date(new Date() - timeCounter))
-    }, 100);
-  });
-
-  console.log(mainTimer);
-
-  return (
-    <div className="App">
-      <p>{formatTime(mainTimer)}</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<MainTimer />
+		</div>
+	);
 }
 
 export default App;
